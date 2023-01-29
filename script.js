37a009ae45f47b74d283937428d472ff272949df
sonia-deni <sonia_deni@hotmail.com>
2023-01-29 00:39:14 +0000

    display the weather for the five day forecast for the city that is selected

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var cities = ["Manchester", "Bangkok", "Przemysl", "Edinburgh", "New York"];
 var APIKey = "fab2f8aed7f362e666aa16a01a05d5ed";
-
+renderButtons();
 
 $( document ).ready(function() {
     
@@ -27,25 +27,17 @@ $( document ).ready(function() {
     });
 }); 
 
-
 function renderButtons() {
   $(".weather-hr").empty();
-
   for (var i = 0; i < cities.length; i++) {
-
     var a = $("<button>");
     a.addClass("city-button");
-   
     a.attr("data-name", cities[i]);
     a.text(cities[i]);
     $(".weather-hr").append(a);
   }
 }
 
-
-
-
-renderButtons();
 
 function getCityWeather(city){
     var queryURLGeo = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" + APIKey;
@@ -54,6 +46,7 @@ function getCityWeather(city){
     url: queryURLGeo,
     method: "GET"
     }).then(function(response) {
+
         lat = response.coord.lat;
         lon = response.coord.lon;
         $('.city-today').text(city);
@@ -65,7 +58,7 @@ function getCityWeather(city){
         $('.temp-today').text("Temperature: " + tempInCelcius + " °C");
         $('.wind-today').text("Wind Speed: " + response.wind.speed + " KPH");
         $('.humidity-today').text("Humidity: " + response.main.humidity + "%");
-console.log(response)
+
         var queryURLWeather = "https://api.openweathermap.org/data/2.5/forecast?lat="+ lat + "&lon="+ lon +"&appid=" + APIKey;
         $.ajax({
             url: queryURLWeather,
@@ -77,21 +70,27 @@ console.log(response)
     });
 }
 
-//need to loop through the array and set the data to each of the textbox.
-function displayWeatherForecast(fiveDay){
-    var weatherDay = []; //create array to hold the weather objects
-   // console.log(fiveDay);
-        for(var i=4; i<fiveDay.length; i= i+8){ 
-            weatherDay[i] = {
-                date: fiveDay[i].dt_txt,
-                icon: fiveDay[i].weather.icon,
-                temp: fiveDay[i].main.temp,
-                wind: fiveDay[i].wind.speed,
-                humidity: fiveDay[i].main.humidity
-            }
 
-    
-            }
-        }
-    
-    
+function displayWeatherForecast(fiveDay){
+    var weatherDay = []; 
+    for (var i=0; i<fiveDay.length; i=i+8){
+        weatherDay.push(fiveDay[i]);
+    }
+
+    for(var i=0; i<weatherDay.length; i++){ 
+        //convert the date to the same format as the date for today's weather
+        var dayDate = weatherDay[i].dt_txt.split(" ");
+        dayDate = dayDate[0].split("-");
+        let year = dayDate[0];
+        let month = dayDate[1];
+        let day = dayDate[2];
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+        $('.date'+i).text(day + " " + months[parseInt(month)-1] + " " + year);
+        var iconAddress = "http://openweathermap.org/img/wn/" + weatherDay[i].weather[0].icon + "@2x.png";
+        $('.icon'+i).attr("src", iconAddress);
+        var tempInCelcius = parseFloat((weatherDay[i].main.temp)-273.15).toFixed(2);
+        $('.temp'+i).text("Temperature: " + tempInCelcius + " °C");
+        $('.wind'+i).text("Wind Speed: " + weatherDay[i].wind.speed + " KPH");
+        $('.humidity'+i).text("Humidity: "+ weatherDay[i].main.humidity + " %");
+    }
+}
