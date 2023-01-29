3b6ce5fd2e801d0ad5285e055cde146327b7d356
sonia-deni <sonia_deni@hotmail.com>
2023-01-29 02:52:43 +0000

    moved the event listener to the render buttons function so that any newly appended buttons are handled correctly. Created README

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# Weather-Dashboard
+# Weather-Dashboard - Bootcamp Week 8
+
+## Server APIs: Weather Dashboard
+
+This project displays the weather for any chosen city, dynamically updating the page with data provided by OpenWeather API. The city can be chosen from existing options, or upon searching for a new city a button is created and appended to the list. This is then saved in local storage.
+
+The app does the following:
+
+When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history.
+
+When a user views the current weather conditions for that city they are presented with:
+
+- The city name
+
+- The date
+
+- An icon representation of weather conditions
+
+- The temperature
+
+- The humidity
+
+- The wind speed
+
+When a user views future weather conditions for that city they are presented with a 5-day forecast that displays:
+
+- The date
+
+- An icon representation of weather conditions
+
+- The temperature
+
+- The humidity
+
+When a user clicks on a city in the search history they are again presented with current and future conditions for that city.
+
+
+## Usage
+
+Here is a mock up of the page:
+
+![screenshot of the dashboard](Screenshot.png)<br>
+
+
+## Credits
+
+[Assignment] https://courses.bootcampspot.com/courses/3021/assignments/44746?module_item_id=832436<br>
+[OpenWeather API] https://openweathermap.org/api
+
+
+## Link to deployed application
+
+https://sonia-deni.github.io/Weather-Dashboard/
+
+## License
+
+This project is licensed under the MIT License
+
+## Badges
+
+![edX](https://img.shields.io/badge/edX-%2302262B.svg?style=for-the-badge&logo=edX&logoColor=white)
+
+![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)
+
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
+
+![jQuery](https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white)
+
+![Bootstrap](https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white)
+
--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 var cities = ["Manchester", "Bangkok", "Przemysl", "Edinburgh", "New York"];
 var APIKey = "fab2f8aed7f362e666aa16a01a05d5ed";
-renderButtons();
-getCityWeather(cities[0]);
-
+renderButtons(); //set initial list of cities to click
+getCityWeather(cities[0]); //set an inital city on page load
 
 $( document ).ready(function() {
     $(".search-button").on("click", function (event) {
         event.preventDefault();
         var city = $('#search-input').val().trim();
-        if(city){
+        if(city){ //check for a city in the text input
             if(!cities.includes(city)){
-                cities.push(city);
+                cities.push(city); //update the array only if the city is not repeated
                 localStorage.setItem("cities", JSON.stringify(cities));
             }
             renderButtons();
@@ -19,24 +18,16 @@ $( document ).ready(function() {
         else{
             alert("Nothing in search box");
         }
-    
-    });
-    
-    $(".city-button").on("click", function (event) {
-        event.preventDefault();
-        var cityClicked = $(this).attr("data-name");
-        getCityWeather(cityClicked);
-        
     });
 });
 
 function renderButtons() {
-  var checkSaved = JSON.parse(localStorage.getItem("cities"));
+  var checkSaved = JSON.parse(localStorage.getItem("cities")); //check if any previously stored cities, and if so, update the array
   if(checkSaved){
     cities = checkSaved;
   }
   $(".weather-hr").empty();
-  
+  //create and append the buttons for each city in the array
   for (var i = 0; i < cities.length; i++) {
     var a = $("<button>");
     a.addClass("city-button");
@@ -44,6 +35,12 @@ function renderButtons() {
     a.text(cities[i]);
     $(".weather-hr").append(a);
   }
+  //add the event listener once the buttons have been appended, so that it is always up to date
+  $(".city-button").on("click", function (event) {
+    event.preventDefault();
+    var cityClicked = $(this).attr("data-name");
+    getCityWeather(cityClicked); 
+});
 }
 
 function getCityWeather(city){
@@ -53,20 +50,22 @@ function getCityWeather(city){
     url: queryURLGeo,
     method: "GET"
     }).then(function(response) {
-
+        //get the lat and lon to use for the 5 day forecast request
         lat = response.coord.lat;
         lon = response.coord.lon;
+        //display the information for today's weather
         $('.city-today').text(city);
         var currentTime = moment().format("DD MMM YYYY");
         $('.date-today').text(currentTime);
         var iconAddress = "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
         $('.icon-today').attr("src", iconAddress);
-        var tempInCelcius = parseFloat((response.main.temp)-273.15).toFixed(2);
+        var tempInCelcius = parseFloat((response.main.temp)-273.15).toFixed(2); //convert to celcius and round to 2.d.p
         $('.temp-today').text("Temperature: " + tempInCelcius + " °C");
         $('.wind-today').text("Wind Speed: " + response.wind.speed + " KPH");
         $('.humidity-today').text("Humidity: " + response.main.humidity + "%");
 
         var queryURLWeather = "https://api.openweathermap.org/data/2.5/forecast?lat="+ lat + "&lon="+ lon +"&appid=" + APIKey;
+        //new query for the full 5 day forecast
         $.ajax({
             url: queryURLWeather,
             method: "GET"
@@ -79,10 +78,11 @@ function getCityWeather(city){
 
 function displayWeatherForecast(fiveDay){
     var weatherDay = []; 
+    //create a new array to hold the data for each of the 5 days, populate it by taking every 8th object from the list of 40
     for (var i=0; i<fiveDay.length; i=i+8){
         weatherDay.push(fiveDay[i]);
     }
-
+    //loop over the 5 day array and update the html with the relevant information
     for(var i=0; i<weatherDay.length; i++){ 
         //convert the date to the same format as the date for today's weather
         var dayDate = weatherDay[i].dt_txt.split(" ");
@@ -92,10 +92,13 @@ function displayWeatherForecast(fiveDay){
         let day = dayDate[2];
         let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
         $('.date'+i).text(day + " " + months[parseInt(month)-1] + " " + year);
+
         var iconAddress = "http://openweathermap.org/img/wn/" + weatherDay[i].weather[0].icon + "@2x.png";
         $('.icon'+i).attr("src", iconAddress);
+
         var tempInCelcius = parseFloat((weatherDay[i].main.temp)-273.15).toFixed(2);
         $('.temp'+i).text("Temperature: " + tempInCelcius + " °C");
+        
         $('.wind'+i).text("Wind Speed: " + weatherDay[i].wind.speed + " KPH");
         $('.humidity'+i).text("Humidity: "+ weatherDay[i].main.humidity + " %");
     }
